e00d890d713a1d675fd5ec3ce8267a0a47f393e2
unknown <karunakarmytthi@gmail.com>
2022-02-08 08:44:48 +0530

    ss

---
+++ hh.java
@@ -0,0 +1,8 @@
+import javax.sound.sampled.SourceDataLine;
+
+class ain{
+    public static void main(String[] args) {
+        System.out.println("a,s,sccgit ");
+        System.out.println("sjsjjssssssssssssssssssssssssssssssssssssssss");
+    }
+}
